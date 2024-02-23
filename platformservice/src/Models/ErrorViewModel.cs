af781bc5170cd83d63de636a53e5fb3cc56f88dd
Mustafa GÜÇ <mustafaguc@Mustafas-MBP.home>
2024-02-23 10:54:14 +0300

    initial commit

---
+++ platformservice/src/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace platformservice.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
